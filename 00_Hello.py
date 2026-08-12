0aef885adfaf7c4bf7435f69571a391d0a91f112
Daveselv3008 <devopsproyectoslfsdeivis@gmail.com>
2026-08-12 13:42:49 -0400

    My first Day with python

---
+++ 00_Hello.py
@@ -0,0 +1,22 @@
+#This is a comment:Impresion de hola mundo= Hello World.
+#Verb TO-BE(Ser o Estar) Presente Simple
+print("I am, You are, He is, She is and It is")
+
+"""
+This is 
+a 
+comment(Three lines)
+
+"""
+
+'''
+My first Practice
+
+'''
+#comando para inferir el tipo de dato en python
+
+print(type("Hello World"))
+print(type(1))
+print(type(1.5))
+print(type(True))
+print(type(3+ 1j))
---
+++ 01_variables.py
@@ -0,0 +1,10 @@
+#Buenas practicas mientras codificas es usar snake_case ejemplo:
+my_name= "My name is Deivis"
+print(my_name)
+
+sujeto_yo = "I"
+sujeto_el = "He"
+sujeto_ella = "She"
+
+print(sujeto_yo,sujeto_el,sujeto_ella)
+
