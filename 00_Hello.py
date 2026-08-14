055baf0ccf53100576eec2d4b4b3fce1bdb44550
Daveselv3008 <devopsproyectoslfsdeivis@gmail.com>
2026-08-13 20:44:07 -0400

    My Second Day with python

--- 00_Hello.py
+++ 00_Hello.py
@@ -15,8 +15,9 @@ My first Practice
 '''
 #comando para inferir el tipo de dato en python
 
-print(type("Hello World"))
-print(type(1))
-print(type(1.5))
-print(type(True))
-print(type(3+ 1j))
+print(type("Hello World"))#tipo String
+print(type(1)) #tipo 'Int'
+print(type(1.5))#tipo 'Float'
+print(type(True))#tipo 'Booleano'
+print(type(3+ 1j))#tipo 'Complex'
+print(type(print((my_name, greetings, sujeto_yo, sujeto_el, sujeto_ella, my_int_to_str_variable,my_bool_variable)))) #tipo 'NoneType'
--- 01_variables.py
+++ 01_variables.py
@@ -8,3 +8,49 @@ sujeto_ella = "She"
 
 print(sujeto_yo,sujeto_el,sujeto_ella)
 
+#Variables
+
+greetings = "Hello, Hi"
+print(greetings)
+
+greetings = 1
+print(greetings)
+
+my_int_to_str_variable = str(greetings)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = False
+print(my_bool_variable)
+
+#Concatenacion de variables en un print
+print(my_name, greetings, sujeto_yo, sujeto_el, sujeto_ella, my_int_to_str_variable,my_bool_variable)
+print(type(print((my_name, greetings, sujeto_yo, sujeto_el, sujeto_ella, my_int_to_str_variable,my_bool_variable))))
+
+#Algunas funciones del sistema
+print(len(sujeto_yo))
+
+print("Titles:", "Mrs,","Mr,","Miss,","Ms.")
+
+#variables en una sola linea:
+name, last_name, age, alias = "Deivis", "Baena", 40, "Daveselv"
+print("My name is:", name,", My lastname is:",last_name, ", I am", age,"years old",", My alias is:", alias)
+
+#inputs (solicita datos )
+"""
+first_name = input("What is your name? ")
+Age = input("How old are you? ")
+print(first_name,age)
+
+"""
+
+#Python es un lenguaje de tipado Dinamico 
+name = 35
+Age = "Deivis"
+
+#Forzamos el tipo? es informativo segun el contexto de trabajo
+
+Greetings: str = "Hello"
+greetings = 1
+print(type(greetings))
+print(greetings)
